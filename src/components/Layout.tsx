6576e71c850302571e899a37c9088d27a8fbe53a
brunopandapacheco <cardapiodigitalpanda@gmail.com>
2026-06-01 23:37:04 -0300

    feat: ícone sino trocado por notifica.png

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -143,7 +143,7 @@ export default function Layout() {
               setNotifCount(0);
               navigate("/notificacoes");
             }} style={{position:"relative"}}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2"><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg>
+            <img src="/notifica.png" alt="Notificações" style={{width:"28px",height:"28px",objectFit:"contain"}} />
             {notifCount > 0 && (
               <span style={{
                 position:"absolute", top:"-4px", right:"-4px",
