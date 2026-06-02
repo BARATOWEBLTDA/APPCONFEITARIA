cc8a3e71bc19276c282e17d32b75506e61acc543
brunopandapacheco <cardapiodigitalpanda@gmail.com>
2026-06-01 23:39:18 -0300

    style: cor do header mobile para #F583BF

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -230,7 +230,7 @@ export default function Layout() {
             position: fixed;
             top: 0; left: 0; right: 0;
             z-index: 9999;
-            background: #331512;
+            background: #F583BF;
             padding: 0.65rem 1.25rem;
             align-items: center;
             justify-content: space-between;
