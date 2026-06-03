78f5e0e7b94ec5493fcfaa5c6b36d701fb52e211
brunopandapacheco <cardapiodigitalpanda@gmail.com>
2026-06-03 17:24:05 -0300

    feat: adiciona SpeedInsights do Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/themes.css'
 import { ThemeProvider } from './context/ThemeContext'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <SpeedInsights />
     </ThemeProvider>
   </StrictMode>,
 )
