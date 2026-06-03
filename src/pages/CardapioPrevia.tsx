63c6863cb6a5a6433314245ea79b7d9606634ae4
brunopandapacheco <cardapiodigitalpanda@gmail.com>
2026-06-03 09:05:27 -0300

    fix: z-index da prévia aumentado para cobrir bottom nav

--- src/pages/CardapioPrevia.tsx
+++ src/pages/CardapioPrevia.tsx
@@ -29,7 +29,7 @@ export default function CardapioPrevia() {
     <div style={{
       position:"fixed",
       top:0, left:0, right:0, bottom:0,
-      zIndex:10,
+      zIndex:200,
       background:"white",
       display:"flex",
       flexDirection:"column"
